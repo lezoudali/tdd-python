381dba7f05c4578dddfbc1ab68a7f121e7931365
Lezou Dali <lezou.dali@gmail.com>
2016-11-12 20:01:08 -0500

    basic view now returns miniaml html

--- functional_tests.py
+++ functional_tests.py
@@ -6,7 +6,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
-        self.browser.implicit_wait(3)
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
