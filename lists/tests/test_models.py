e5dc425de0ea3c0c310e41727705c1f6c1d75b0a
Lezou Dali <lezou.dali@gmail.com>
2016-11-26 16:11:52 -0500

    Refactor hard-coded URLs out of templates

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -38,3 +38,7 @@ class ListAndItemModelTest(TestCase):
         with self.assertRaises(ValidationError):
             item.save()
             item.full_clean()
+
+    def test_get_absolute_url(self):
+        list_ = List.objects.create()
+        self.assertEqual(list_.test_get_absolute_url(), '/lists/{}/'.format(list_.id))
